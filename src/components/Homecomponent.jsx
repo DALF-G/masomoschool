a4dc3cca345d8e6c851dd1c960a6a19077af50d5
dalf-g <muriithi.andrie@gmail.com>
2025-11-05 16:30:41 +0300

    first

---
+++ src/components/Homecomponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homecomponent = () => {
+  return (
+    <div>
+      <h1 className='text-center text-primary'>Welcome to the Home Page of Masomo Plus</h1>
+    </div>
+  )
+}
+
+export default Homecomponent
