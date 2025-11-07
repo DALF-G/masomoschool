27b537a5827cf62c4fb238c7433d305463c35a78
dalf-g <muriithi.andrie@gmail.com>
2025-11-07 16:21:59 +0300

    new code

--- src/components/Homecomponent.jsx
+++ src/components/Homecomponent.jsx
@@ -1,32 +1,13 @@
 import React from 'react'
 import "../components/css/home.css"
 import { Link } from 'react-router-dom'
+import OurNavbar from './OurNavbar'
 
 const Homecomponent = () => {
   return (
     <div className='homepage'>
-      {/* navbar */}
-      <nav className='navbar navbar-expand-md navbar-dark bg-success'>
-        <div className="container">
-          <Link className='navbar-brand' to={'/'}>Masomo Plus School</Link>
-
-          {/* below is the toggle button - either to expard or collapse contents of the navbar */}
-          <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navbarnav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          {/* Below is the div that carries all the link to different pages */}
-          <div className= "collapse navbar-collapse justify-content-end" id='navbarnav'>
-            <ul className='navbar-nav'>
-              <li className='nav-item'><Link to={"/"} className='nav-link active'>Home</Link></li>
-              <li className='nav-item'><a href="#About" className="nav-link">About</a></li>
-              <li className='nav-item'><a href="#cbc" className="nav-link">CBC-Curriculum</a></li>
-              <li className='nav-item'><a href="#WhyMasomoPlus" className="nav-link">Why-us?</a></li>
-              <li className='nav-item'><Link to={"/login"} className='nav-link'>Login</Link></li>
-            </ul>
-
-          </div>
-        </div>
-      </nav>
+      {/* Below we Render our navbar */}
+      <OurNavbar/>
       
       {/* Below is the hero section */}
       <section className="hero position-relative text-white">
--- src/components/Registercomponent.jsx
+++ src/components/Registercomponent.jsx
@@ -1,9 +1,141 @@
-import React from 'react'
+import React, { useState } from 'react'
+import OurNavbar from './OurNavbar'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 const Registercomponent = () => {
+  // declare hooks to hold the values inserted by the user
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [secretkey, setSecretkey] = useState("");
+
+  // console.log(name)
+
+  // capture the states your applicatin moves in.
+  const [loading, setLoading] = useState("");
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
+  // Declare the navigate hook such that if a person successfully register, he can be redirected to login page
+  const navigate = useNavigate();
+
+  // specify the url/api endpoin where the dat will go to
+  const url = "https://kindergartenapi-olyn.onrender.com/api/admin/register"
+
+  // Below is the function to handle the submit event in registering an Admin
+  const handleSubmit = async (e) =>{
+    // prevent the site from reloading when the register button is clicked
+    e.preventDefault()
+
+    // Update the losding hook with a message
+    setLoading("Admin registration in progress.. please wait..")
+
+    try{
+      // create a form data object to hold all the values
+      const data = {name, email, password, secretkey};
+
+      const res = await axios.post(url, data)
+
+      console.log("Registration Success:", res.data)
+
+      // Update the loading hook back to default
+      setLoading("")
+
+      // update the success hook with a message
+      setSuccess(res.data.message)
+
+      alert("Registration Successful")
+      navigate("/login");
+
+
+      // clear the hooks
+      setName("");
+      setEmail("");
+      setPassword("");
+      setSecretkey("");
+    }
+    catch(err){
+      // if there is an error, Update the loading hook back to empty
+      setLoading("");
+
+      // then update the error hook with the error meesage
+      setError("Oooops.. Registration Failed. Please try again...")
+    }
+
+  }
+
+
   return (
     <div>
-      <h1>Create a New Account..</h1>
+      <OurNavbar/>
+      <div className="container mt-5">
+       <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-6">
+          <form onSubmit={handleSubmit} className="card shadow p-4 bg-light rounded">
+          <h1 className='text-center text-success'>Masomo Plus School</h1>
+          <h2 className='text-center text-success mb-4'>Admin Register</h2>
+
+          {loading ? <div className='alert alert-info'>{loading}</div> : null}
+          {error ? <div className='alert alert-danger'>{error}</div> : null}
+          {success ? <div className='alert alert-success'>{success}</div> : null}
+
+          <input 
+          type="text" 
+          placeholder="Enter Your Name Here" 
+          className='form-control mb-3'
+          required
+          value={name}
+          onChange={(e) => {setName(e.target.value)}}
+          />
+          {/* {name} */}
+
+          <input type="email" 
+          placeholder='Enter Your Email Here'
+          className="form-control mb-3" 
+          required
+          value={email}
+          onChange={(e) => {setEmail(e.target.value)}}
+          />
+          {/* {email} */}
+
+          <input type="password" 
+          placeholder='Enter Your Password Here'
+          className="form-control mb-3"
+          required
+          value={password}
+          onChange={(e) => {setPassword(e.target.value)}}
+           />
+           {/* {password} */}
+
+           <input type="password" 
+           placeholder='Enter Your Admin secretkey Here'
+           className="form-control mb-3" 
+           required
+           value={secretkey}
+          onChange={(e) => {setSecretkey(e.target.value)}}
+           />
+           {/* {secretKey} */}
+           <br />
+
+           <div className="d-grid mb-3">
+            <button type="submit " className="btn btn-outline-success">Register</button>
+           </div>
+           <div className="text-center">
+            <p>Already Have an Account? {' '} 
+              <Link to={"/login"} className='text-decoration-none'>Login
+              </Link>
+
+            </p>
+           </div>
+
+
+        </form>
+        </div>
+        <div className="col-md-3"></div>
+        </div> 
+      </div>
     </div>
   )
 }
